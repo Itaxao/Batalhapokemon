0029163dcd88ef15b86a3626faa2d8e7e7d1222c
Itaxao <xavieritalo853@gmail.com>
2025-12-17 09:36:44 -0300

    arrumado blastoise

--- BatalhaPokemon/src/br/com/cefetmg/batalhapokemon/model/agua/squirtle/Blastoise.java
+++ BatalhaPokemon/src/br/com/cefetmg/batalhapokemon/model/agua/squirtle/Blastoise.java
@@ -1,40 +1,35 @@
 package br.com.cefetmg.batalhapokemon.model.agua.squirtle;
+
 import br.com.cefetmg.batalhapokemon.model.Pokemon;
 import br.com.cefetmg.batalhapokemon.model.enums.Sexo;
 import br.com.cefetmg.batalhapokemon.model.enums.Tipo;
 import br.com.cefetmg.batalhapokemon.model.dtos.Ataque;
 
-public class Blastoise extends Wartortle {
+public class Blastoise extends Pokemon {
 
-    public Blastoise(String apelido){
-        super(apelido);
-        this.especie = "Blastoise";
-        this.nivelEvolucao = 3;
-        this.vidaMaxima = 79; this.ataque = 83; this.defesa = 100; this.ataqueEsp = 85; this.defesaEsp = 105; this.velocidade = 78;
-
-        adicionarAtaque( new Ataque("Water Pulse",60, Tipo.AGUA));
-        adicionarAtaque( new Ataque("Protect",0, Tipo.NORMAL));
-        adicionarAtaque( new Ataque("Hydro Pump",110, Tipo.AGUA));
-        adicionarAtaque( new Ataque("Shell Smash", 0, Tipo.AGUA));
+    public Blastoise(String apelido) {
+        super(apelido, "Blastoise", Tipo.AGUA, 3, 79, 83, 85, 100, 105, 78);
+        adicionarAtaque(new Ataque("Water Pulse", 60, Tipo.AGUA));
+        adicionarAtaque(new Ataque("Protect", 0, Tipo.NORMAL));
+        adicionarAtaque(new Ataque("Hydro Pump", 110, Tipo.AGUA));
+        adicionarAtaque(new Ataque("Shell Smash", 0, Tipo.AGUA));
     }
 
     protected Blastoise(String apelido, Sexo sexo) {
-        super(apelido);
-        this.sexo = sexo;
-        this.especie = "Blastoise";
-        this.nivelEvolucao = 3;
-        this.vidaMaxima = 79; this.ataque = 83; this.defesa = 100; this.ataqueEsp = 85; this.defesaEsp = 105; this.velocidade = 78;
-
-        adicionarAtaque( new Ataque("Water Pulse",60, Tipo.AGUA));
-        adicionarAtaque( new Ataque("Protect",0, Tipo.NORMAL));
-        adicionarAtaque( new Ataque("Hydro Pump",110, Tipo.AGUA));
-        adicionarAtaque( new Ataque("Shell Smash", 0, Tipo.AGUA));
+        super(apelido, "Blastoise", Tipo.AGUA, sexo, 3, 79, 83, 85, 100, 105, 78);
+        adicionarAtaque(new Ataque("Water Pulse", 60, Tipo.AGUA));
+        adicionarAtaque(new Ataque("Protect", 0, Tipo.NORMAL));
+        adicionarAtaque(new Ataque("Hydro Pump", 110, Tipo.AGUA));
+        adicionarAtaque(new Ataque("Shell Smash", 0, Tipo.AGUA));
     }
 
     @Override
-    public Pokemon tentarEvoluir(){ return this; }
+    public Pokemon evoluir() {
+        return this; // Blastoise é a evolução final, não evolui mais
+    }
 
-    public void realizarSom(){
+    @Override
+    public void realizarSom() {
         System.out.println("Blastoise! Blastoise!");
     }
 }
